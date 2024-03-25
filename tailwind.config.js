9d2a3cb5adf26f83d8ab0ffabb7b0537a53796fa
Le_Van_Huy_Hoang <lvhhoangg171@gmail.com>
2024-03-25 16:27:37 +0700

    Add product magazine

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        xs: "480px",
+      },
       fontFamily: {
         Karlar: ["Karla", "sans-serif"],
       },
